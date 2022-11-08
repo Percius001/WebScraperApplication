2cafb0078b9d9f4ee4ff1b9dee601d638ff65203
Sumit <48628948+Percius001@users.noreply.github.com>
2022-11-08 13:52:29 +0530

    Add files via upload

---
+++ WebScraperApplication/WebScraperApplication/Logic/WebScrapperLogic.cs
@@ -0,0 +1,90 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebScraperApplication.Models;
+
+namespace WebScraperApplication.Logic
+{
+    //using singleton desing pattern
+    public sealed class WebScrapperLogic
+    {
+        private static readonly WebScrapperLogic webScrapperLogic = new WebScrapperLogic();
+        private WebScrapperLogic()
+        { }
+        public static WebScrapperLogic Instance()
+            { return webScrapperLogic; }
+
+        //gets the response from the webpages
+        public async Task<List<JobInfo>> GetDataFromWebPage()
+        {
+            List<JobInfo> data = new List<JobInfo>();
+            string url = "https://boards.greenhouse.io/embed/job_board?for=coursera";
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(url);
+            var jobList = ParseHTMLJobs(response); 
+            foreach (var jobId in jobList)
+            {
+                response = await httpClient.GetStringAsync("https://boards.greenhouse.io/embed/job_app?for=coursera&token="+jobId);
+                JobInfo jobInfo = ParseHTMLJobInfo(response);
+                data.Add(jobInfo);
+            }
+            return data;
+        }
+
+        //parse the html string to get the job ids.
+        public List<string> ParseHTMLJobs(string htmlData)
+        {
+            List<string> result = new List<string>();   
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlData);
+            var jobLinks = htmlDocument.DocumentNode.Descendants("a")
+                            .Where(node => node.GetAttributeValue("href","").Contains("jobs?gh_jid"))
+                            .ToList();
+            foreach(var link in jobLinks)
+            {
+                if (link.Attributes.Count > 0)
+                    result.Add(link.Attributes[2].Value.Substring(link.Attributes[2].Value.IndexOf("=")+1));
+            }
+            return result;
+        }
+
+        //parse the html string to get all the information from a job.
+        public JobInfo ParseHTMLJobInfo(string htmlData)
+        {
+            JobInfo jobInfo = new JobInfo();
+            int des = 0,res = 0,bq = 0,pq = 0;
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlData);
+            var header = htmlDocument.DocumentNode.Descendants("div")
+                            .Where(node => node.GetAttributeValue("id", "").Contains("header"))
+                            .ToList();
+            jobInfo.Title = header[0].ChildNodes[1].InnerText.Replace(@"&amp;", "&");
+            jobInfo.Location = Regex.Replace(header[0].ChildNodes[5].InnerText, @"\t|\n|\r", "").Trim();
+            var content = htmlDocument.DocumentNode.Descendants("div")
+                            .Where(node => node.GetAttributeValue("id", "").Contains("content"))
+                            .ToList();
+            
+            for(int i = 0; i < content[0].ChildNodes.Count; i++)
+            {
+                if (content[0].ChildNodes[i].InnerText.Contains("Job Overview"))
+                    des = i + 2;
+                if (content[0].ChildNodes[i].InnerText.Contains("Responsibilities"))
+                    res = i + 2;
+                if (content[0].ChildNodes[i].InnerText.Contains("Basic Qualifications") || content[0].ChildNodes[i].InnerText.Contains("Your Skills"))
+                    bq = i + 2;
+                if (content[0].ChildNodes[i].InnerText.Contains("Preferred Qualifications"))
+                    pq = i + 2;
+            }
+            jobInfo.Description = Regex.Replace(content[0].ChildNodes[des].InnerText, @"&amp;","&").Trim();
+            jobInfo.Responsibilites = Regex.Replace(content[0].ChildNodes[res].InnerText, @"&amp;", "&").Trim().Split("\n").ToList();
+            jobInfo.BasicQualifications = Regex.Replace(content[0].ChildNodes[bq].InnerText, @"&amp;", "&").Trim().Split("\n").ToList();
+            jobInfo.PreferredQualifications = Regex.Replace(content[0].ChildNodes[pq].InnerText, @"&amp;", "&").Trim().Split("\n").ToList();
+            return jobInfo;
+        }
+
+    }
+}
---
+++ WebScraperApplication/WebScraperApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WebScraperApplication.Logic;
+using WebScraperApplication.Models;
+
+//singleton object creation.
+var result = WebScrapperLogic.Instance();
+
+//function call to take the job infos from the webpage. 
+var jobInfos = await result.GetDataFromWebPage();
+
+//Output Displaying the job data.
+foreach(JobInfo job in jobInfos)
+{
+    Console.Write("Job Title: ");
+    Console.WriteLine(job.Title);
+    Console.WriteLine();
+    Console.WriteLine("Job Descirption:");
+    Console.WriteLine(job.Description);
+    Console.WriteLine();
+    Console.WriteLine("Responsibilites");
+    foreach(string res in job.Responsibilites)
+        Console.WriteLine("-> "+res);
+    Console.WriteLine();
+    Console.WriteLine("Basic Qualifications:");
+    foreach (string res in job.BasicQualifications)
+        Console.WriteLine("-> " + res);
+    Console.WriteLine();
+    Console.WriteLine("Preffered Qualifications:");
+    foreach (string res in job.PreferredQualifications)
+        Console.WriteLine("-> " + res);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.ReadLine();
